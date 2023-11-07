0af7f96d6232acd7312ca795bf9883a07a8ae8e9
14months <waysoleward01@naver.com>
2023-11-07 21:15:57 +0900

    게시글 라우터 설정

---
+++ routers/post.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from starlette import status
+
+from db import db_post
+from db.database import get_db
+from routers.schemas import PostBase, PostDisplay
+
+router = APIRouter(
+    prefix='/post',
+    tags=['post'],
+)
+
+image_url_types = ['absolute', 'relative']
+
+
+@router.post('', response_model=PostDisplay)
+def create(request: PostBase, db: Session = Depends(get_db)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="이미지 URL 타입은 절대 경로나 상대 경로 입니다.")
+    return db_post.create(db, request)
