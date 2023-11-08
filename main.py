fcac2f4fa02a45c4519e346c54dfab4d9684f0a7
14months <waysoleward01@naver.com>
2023-11-08 23:16:23 +0900

    CORS 설정

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from auth import authentication
 from db import models
@@ -6,13 +7,26 @@ from db.database import engine
 from routers import user, post, comment
 from fastapi.staticfiles import StaticFiles
 
-
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(comment.router)
 app.include_router(authentication.router)
+
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 @app.get("")
 def root():
