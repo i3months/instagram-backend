c8c8a14b218b102b4557df873ae5876451361cef
14months <waysoleward01@naver.com>
2023-11-08 22:24:16 +0900

    게시글 삭제

--- db/db_post.py
+++ db/db_post.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
+from fastapi import HTTPException
 from sqlalchemy.orm.session import Session
+from starlette import status
+
 from routers.schemas import PostBase
 from db.models import DbPost
 
@@ -20,3 +23,15 @@ def create(db: Session, request: PostBase):
 
 def get_all(db: Session):
     return db.query(DbPost).all()
+
+
+def delete(db: Session, id: int, user_id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'{id} 게시글이 없습니다.')
+    if post.user_id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f'작성자만 삭제할 수 있습니다.')
+
+    db.delete(post)
+    db.commit()
+    return 'ok'
